a6da915d159a7063e5e5234d52be0ca3fe9ecc7c
andi-goldfish <andreaesime.azca@gmail.com>
2024-08-07 01:06:00 -0600

    cambios

---
+++ README.md
@@ -0,0 +1,24 @@
+# Portafolio de habilidades Technolochicas Pro
+
+El presente proyecto es un portafolio desarrollado para poner en práctica las habilidades obtenidad dentro del bootcamp de desarrollo frontend de Techno lochicas PRO.
+
+Fue desarrollado con HTML, CSS. y J5 con el uso de el framework de UI, Bootstrap utilizando además bibliotecas externas.
+
+La página es responsiva (adaptable a diferentes tamaños de pantalla) e incluye la presentación de la autora del proyecto.
+
+[Proyecto Desplegado (https://portafolio-eta-blue.vercel.app/)](https://portafolio-eta-blue.vercel.app/)
+
+## Secciones de mi sitio
+
+![Presentacion]()
+
+## Tecnologías
+
+* HTML
+* CSS
+* Bootstrap
+* Javascript
+
+---
+
+Desarrollado con <3 por Andrea López en [Technolochicas Pro.](https://tecnolochicas.mx/)
---
+++ script.js
@@ -0,0 +1,56 @@
+
+let app = document.getElementById('app');
+
+let typewriter = new Typewriter(app, {
+    loop: true,
+    delay: 75,
+});
+
+//una acccion tiene parentesis despues del nombre de la accion
+//a veces tiene el operador punto
+
+typewriter
+    .pauseFor(1500) //ms
+    .typeString('Hola, soy Andrea López')
+    .pauseFor(300)
+    .deleteAll()
+    .typeString('Desarrolladora Frontend Jr.')
+    .pauseFor(1000)
+    .start();
+
+
+
+let audioElement; // Variable para almacenar el objeto Audio
+const mainElement = document.querySelector('main'); // Seleccionar la etiqueta <main>
+mainElement.addEventListener('click', function () {
+
+    if (!audioElement) {
+        audioElement = new Audio('assets/music/under.mp3');
+        audioElement.volume = 0.1;
+    }
+
+    audioElement.play();
+});
+
+const pauseButton = document.getElementById('pauseButton');
+
+pauseButton.addEventListener('click', function () {
+    if (audioElement.paused) {
+        audioElement.play();
+        pauseButton.textContent = "Pausar";
+    } else {
+        audioElement.pause();
+        pauseButton.textContent = "Reanudar";
+    }
+});
+
+
+
+async function copiarAlPortapapeles(texto) {
+    try {
+        await navigator.clipboard.writeText(texto);
+        console.log('Texto copiado al portapapeles:', texto);
+    } catch (err) {
+        console.error('Error al copiar:', err);
+    }
+}
